63818b9b3142c48fe54aeaf48d4127fbe3a4d61d
Nadwey <81181783+Nadwey@users.noreply.github.com>
2022-03-07 18:39:35 +0100

    Change Java library
    new Class: PDRectangle
    Fixed bugs

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+const Color = require("./lib/Color");
 const Matrix = require("./lib/Matrix");
 const PDDocument = require("./lib/PDDocument");
 const PDDocumentInformation = require("./lib/PDDocumentInformation");
 const PDPage = require("./lib/PDPage");
 const PDPageContentStream = require("./lib/PDPageContentStream");
 const PDPageTree = require("./lib/PDPageTree");
+const PDRectangle = require("./lib/PDRectangle");
 const PDType1Font = require("./lib/PDType1Font");
 const Vector = require("./lib/Vector");
 
 module.exports = {
+    Color,
     Matrix,
     PDDocument,
     PDDocumentInformation,
     PDPage,
     PDPageContentStream,
     PDPageTree,
+    PDRectangle,
     PDType1Font,
     Vector,
 };
--- testFile.js
+++ testFile.js
@@ -1,19 +1,44 @@
-const { PDDocument, PDPage, PDPageContentStream, PDType1Font } = require("./index");
+const {
+    PDDocument,
+    PDPage,
+    PDPageContentStream,
+    PDType1Font,
+    Color,
+    PDRectangle,
+} = require("./index");
 const fs = require("fs");
+const { end } = require("./lib/javaInit");
+
+// let doc = PDDocument.loadFromPath("./doc.pdf");
+// let info = doc.getDocumentInformation();
+// console.log(info.getCreationDate());
+
+// info.setCreationDate(new Date(16453));
+
+// doc.saveToPath("./doc.pdf");
+// doc.close();
 
 let doc = PDDocument.create();
-let page = PDPage.create();
+let rect = PDRectangle.create(1080, 256 * 50);
+let page = PDPage.create(rect);
 doc.addPage(page);
 
 let contentStream = PDPageContentStream.create(doc, page);
 contentStream.setFont(PDType1Font.HELVETICA, 40);
-contentStream.beginText();
-contentStream.newLineAtOffset(190, 400);
-contentStream.showText("pdfbox-node");
-contentStream.endText();
+
+for (let i = 0; i < 255; i++) {
+    let color = Color.createRGB((255 - i) / 255, i / 255, 40 / 255);
+    contentStream.setNonStrokingColor(color);
+    contentStream.beginText();
+    contentStream.newLineAtOffset(Math.sin(i / 10) * 100 + 190, i * 50 + 20);
+    contentStream.showText("pdfbox-node");
+    contentStream.endText();
+}
+
 contentStream.close();
 
 doc.saveToPath("./doc.pdf");
 doc.close();
 
+end();
 process.exit();
