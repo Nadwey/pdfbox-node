e4236f0dbd310b28f485a3f77f5f144b1e55e8c0
Nadwey <81181783+Nadwey@users.noreply.github.com>
2022-03-11 18:53:49 +0100

    Async function + more tests

--- test.js
+++ test.js
@@ -7,10 +7,27 @@ const {
     PDDocumentInformation,
     PDPageTree,
 } = require("./index");
+const path = require("path");
 
 describe("PDDocument", () => {
     test("create", () => {
         expect(PDDocument.create()).toBeInstanceOf(PDDocument);
+    });
+
+    test("loadFromPath", () => {
+        let doc = PDDocument.loadFromPath(path.join(__dirname, "./testDoc.pdf"));
+        expect(doc.getPage(0)).toBeInstanceOf(PDPage);
+        expect(() => {
+            doc.getPage(1);
+        }).toThrow();
+    });
+
+    test("loadFromPathAsync", async () => {
+        let doc = await PDDocument.loadFromPathAsync(path.join(__dirname, "./testDoc.pdf"));
+        expect(doc.getPage(0)).toBeInstanceOf(PDPage);
+        expect(() => {
+            doc.getPage(1);
+        }).toThrow();
     });
 
     test("addPage", () => {
@@ -22,9 +39,23 @@ describe("PDDocument", () => {
         expect(doc.getNumberOfPages()).toBe(1);
     });
 
+    test("addPageAsync", async () => {
+        let doc = PDDocument.create();
+        let page = PDPage.create(PDRectangle.LETTER);
+        expect(page).toBeInstanceOf(PDPage);
+        expect(doc.getNumberOfPages()).toBe(0);
+        await doc.addPageAsync(page);
+        expect(doc.getNumberOfPages()).toBe(1);
+    });
+
     test("getDocumentInformation", () => {
         let doc = PDDocument.create();
         expect(doc.getDocumentInformation()).toBeInstanceOf(PDDocumentInformation);
+    });
+
+    test("getDocumentInformationAsync", async () => {
+        let doc = PDDocument.create();
+        expect(await doc.getDocumentInformationAsync()).toBeInstanceOf(PDDocumentInformation);
     });
 
     test("getNumberOfPages", () => {
@@ -32,6 +63,13 @@ describe("PDDocument", () => {
         expect(doc.getNumberOfPages()).toBe(0);
         doc.addPage(PDPage.create(PDRectangle.LETTER));
         expect(doc.getNumberOfPages()).toBe(1);
+    });
+
+    test("getNumberOfPagesAsync", async () => {
+        let doc = PDDocument.create();
+        expect(await doc.getNumberOfPagesAsync()).toBe(0);
+        doc.addPage(PDPage.create(PDRectangle.LETTER));
+        expect(await doc.getNumberOfPagesAsync()).toBe(1);
     });
 
     test("getPage", () => {
@@ -44,9 +82,24 @@ describe("PDDocument", () => {
         }).toThrow();
     });
 
+    test("getPageAsync", async () => {
+        let doc = PDDocument.create();
+        let page = PDPage.create(PDRectangle.LETTER);
+        doc.addPage(page);
+        expect(await doc.getPageAsync(0)).toBeInstanceOf(PDPage);
+        await expect(async () => {
+            await doc.getPageAsync(1);
+        }).rejects.toThrow();
+    });
+
     test("getPages", () => {
         let doc = PDDocument.create();
         expect(doc.getPages()).toBeInstanceOf(PDPageTree);
+    });
+
+    test("getPagesAsync", async () => {
+        let doc = PDDocument.create();
+        expect(await doc.getPagesAsync()).toBeInstanceOf(PDPageTree);
     });
 
     test("importPage - TODO", () => {
@@ -63,17 +116,34 @@ describe("PDDocument", () => {
         }).toThrow();
     });
 
+    test("importPageAsync - TODO", async () => {
+        let docIn = PDDocument.create();
+        docIn.addPage(PDPage.create(PDRectangle.LEGAL));
+
+        let docOut = PDDocument.create();
+        let importedPage = await docOut.importPageAsync(docIn.getPage(0));
+        docIn.removePage(0);
+        // TODO, better checking
+        expect(importedPage).toBeInstanceOf(PDPage);
+        expect(() => {
+            docIn.getPage(0);
+        }).toThrow();
+    });
+
     test("isEncrypted - TODO", () => {
+        expect(true).toBe(true);
+    });
+
+    test("isEncryptedAsync - TODO", async () => {
         expect(true).toBe(true);
     });
 
     test("removePage", () => {
         let doc = PDDocument.create();
-
         expect(() => {
             doc.removePage(0);
         }).toThrow();
-        doc.addPage(PDPage.create());
+        doc.addPage(PDPage.create(PDRectangle.LEGAL));
         expect(() => {
             doc.removePage(0);
         }).not.toThrow();
@@ -82,16 +152,41 @@ describe("PDDocument", () => {
         }).toThrow();
     });
 
-    test('saveToPath', () => {
+    test("removePageAsync", async () => {
+        let doc = PDDocument.create();
+        await expect(async () => {
+            await doc.removePageAsync(0);
+        }).rejects.toThrow();
+        doc.addPage(PDPage.create(PDRectangle.LEGAL));
+        await doc.removePageAsync(0); // should not throw
+        await expect(async () => {
+            await doc.removePageAsync(0);
+        }).rejects.toThrow();
+    });
+
+    test("saveToPath", () => {
         let working = this;
         expect(this).toBe(working);
     });
 
-    test('setDocumentInformation', () => { 
+    test("saveToPathAsync", () => {
+        let working = this;
+        expect(this).toBe(working);
+    });
+
+    test("setDocumentInformation", () => {
         let doc = PDDocument.create();
         let info = PDDocumentInformation.create();
         info.setAuthor("testing123");
         doc.setDocumentInformation(info);
+        expect(doc.getDocumentInformation().getAuthor()).toBe("testing123");
+    });
+
+    test("setDocumentInformationAsync", async () => {
+        let doc = PDDocument.create();
+        let info = PDDocumentInformation.create();
+        info.setAuthor("testing123");
+        await doc.setDocumentInformationAsync(info);
         expect(doc.getDocumentInformation().getAuthor()).toBe("testing123");
     });
 });
